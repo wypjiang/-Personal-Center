092eab24c0a666764ca4f7e6704210fc9f361a3d
wypjiang <784083891@qq.com>
2024-11-06 14:30:49 +0800

    默认变更列表

---
+++ src/components/MoreContent.vue
@@ -0,0 +1,27 @@
+<template>
+  <div></div>
+  <input v-model="value"  class="more-content" placeholder="您可在此编写任意内容" type="text"/>
+</template>
+
+<style lang="scss" scoped>
+.more-content {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: none;
+  margin-top: 20px;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.2509803922);
+  backdrop-filter: blur(10px);
+  
+}
+input:focus{
+    outline: none;
+  }
+</style>
+
+<script setup>
+import { ref } from 'vue';
+const value = ref('')
+</script>
